f0b3114e8c665173c63053a313a90d9cdac64dd0
hsn771 <hr492785@gmail.com>
2025-10-11 11:49:28 +0600

    update Customer panel login

--- resources/views/customer_panel/auth/login.blade.php
+++ resources/views/customer_panel/auth/login.blade.php
@@ -149,10 +149,10 @@
                     </div>
 
                     {{-- Replace with this simpler version: --}}
-                        <div class="mb-3 form-check">
+                        <!--<div class="mb-3 form-check">
                             <input type="checkbox" class="form-check-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                             <label class="form-check-label" for="remember">Remember Me</label>
-                        </div>
+                        </div>-->
 
 
 
